9ab0d7320ae0a9b6800a3ce87c4cefb9f3e7fb2c
Yue Zhang <yue.zhang@emerson.com>
2016-12-06 11:38:04 +0000

    word count exercise

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,2 +1,17 @@
-public class WordCount {
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
+public final class WordCount {
+    public static Map<String, Integer> phrase(String sentence)
+    {
+        final String[] words = sentence.replaceAll( "[\\W&&[\\S]]", "" ).split( " " );
+        final Map< String, Integer > wordCount = new HashMap<>();
+        Arrays.stream( words ).filter( word -> !word.isEmpty() )
+            .map( String::toLowerCase )
+            .forEach( word -> wordCount.put( word, wordCount.keySet().contains( word )
+                ? wordCount.get( word ) + 1
+                : 1 ) );
+        return wordCount;
+    }
 }
